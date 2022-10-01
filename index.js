d82307e69b9cb2e656b2dad11726e5b8262c9015
Stefan Nienhuis <stefan@nienhuisdevelopment.com>
2022-10-01 10:52:51 +0200

    Implement / endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let express = require('express');
 
 let app = express();
 
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
 app.listen(3000, () => {
     console.log('Started listening on port 3000');
 });
