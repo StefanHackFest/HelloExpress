11ea4105557e19995e87df519debc0df5d015513
Stefan Nienhuis <stefan@nienhuisdevelopment.com>
2022-10-01 10:53:49 +0200

    Add /ping endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
     response.send('Hello World!');
 });
 
+app.get('/ping', (request, response) => {
+    response.send('Pong');
+})
+
 app.listen(3000, () => {
     console.log('Started listening on port 3000');
 });
